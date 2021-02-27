9279e22a4de08ebf783d727f574d1b63048acce2
Yangmu Jiang <celsius.j@gmail.com>
2021-02-26 21:55:44 -0800

    types: add tuple struct, enum.

--- HelloTypes/HelloTypes.rs
+++ HelloTypes/HelloTypes.rs
@@ -1,21 +1,21 @@
 use std::fmt;
 
-struct Point {
-	x : i32,
-	y : i32,
-}
+struct Point { x : i32, y : i32, }
+struct Pair ( i32, i32 );
+#[allow(dead_code)]
+enum Color { WHITE, BLACK }
 
-impl fmt::Display for Point {
-	fn fmt(&self, fmt: &mut fmt::Formatter<'_>) -> fmt::Result {
-		write!(fmt, "({}, {})", self.x, self.y)
-	}
-}
 impl fmt::Debug for Point {
 	fn fmt(&self, fmt: &mut fmt::Formatter<'_>) -> fmt::Result {
 		fmt.debug_struct("Point")
 		.field("x", &self.x)
 		.field("y", &self.y)
 		.finish()
+	}
+}
+impl fmt::Display for Pair {
+	fn fmt(&self, fmt: &mut fmt::Formatter<'_>) -> fmt::Result {
+		write!(fmt, "({}, {})", self.0, self.1)
 	}
 }
 
@@ -26,7 +26,9 @@ fn main() {
 	let btype : bool = true;
 	let tuptype : (char, u32) = ('键', 2);
 	let arrtype : [&str; 7] = ["shoot"; 7];
-	let pttype : Point = Point { x: -200, y: 200 };
+	let sttype1 : Point = Point { x: -200, y: 200 };
+	let sttype2 : Pair = Pair(100, 200);
+	let etype1 : Color = Color::BLACK;
 
 	println!("itype = {}", itype);	
 	println!("utype = {}", utype);	
@@ -34,5 +36,8 @@ fn main() {
 	println!("btype = {}", btype);
 	println!("tuptype = {:?}\n\tfirst item: {}", tuptype, tuptype.0);
 	println!("arrtype = {:?}\n\tfirst item: {}", arrtype, arrtype[0]);
-	println!("pttype = {} {:?}", pttype, pttype);
+	println!("sttype1 = {:?}", sttype1);
+	println!("sttype2 = {}", sttype2);
+	println!("sttype3 = {}", sttype3);
+	println!("etype1 = {:?}", match etype1 { Color::WHITE => "WHITE", _ => "not WHITE"});
 }
