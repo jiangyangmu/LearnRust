74d36e37fe7f81df9fb8955205c1ceee6697597c
Yangmu Jiang <celsius.j@gmail.com>
2021-02-27 00:19:22 -0800

    list: add SList iterators.

--- list/src/slist.rs
+++ list/src/slist.rs
@@ -2,6 +2,16 @@ pub struct SList<T> {
 	head : SLink<T>,
 }
 
+pub struct IntoIter<T>{
+	next : SList<T>
+}
+pub struct Iter<'a, T> {
+	next : Option<&'a SNode<T>>
+}
+pub struct IterMut<'a, T> {
+	next : Option<&'a mut SNode<T>>
+}
+
 type SLink<T> = Option<Box<SNode<T>>>;
 
 struct SNode<T> {
@@ -45,8 +55,46 @@ impl<T> SList<T> {
 			Some(node) => Some(&mut node.elem),
 		}
 	}
+
+	pub fn into_iter(self) -> IntoIter<T> {
+		IntoIter { next: self }
+	}
+
+	pub fn iter(&self) -> Iter<'_, T> {
+		Iter { next: self.head.as_deref() }
+	}
+
+	pub fn iter_mut(&mut self) -> IterMut<'_, T> {
+		IterMut { next: self.head.as_deref_mut() }
+	}
 }
 
+impl<T> Iterator for IntoIter<T> {
+	type Item = T;
+	fn next(&mut self) -> Option<Self::Item> {
+		self.next.pop()
+	}
+}
+
+impl<'a, T> Iterator for Iter<'a, T> {
+	type Item = &'a T;
+	fn next(&mut self) -> Option<Self::Item> {
+		self.next.map(|node| {
+			self.next = node.next.as_deref();
+			&node.elem
+		})
+	}
+}
+
+impl<'a, T> Iterator for IterMut<'a, T> {
+	type Item = &'a mut T;
+	fn next(&mut self) -> Option<Self::Item> {
+		self.next.take().map(|node| {
+			self.next = node.next.as_deref_mut();
+			&mut node.elem
+		})
+	}
+}
 
 #[cfg(test)]
 mod tests {
@@ -84,5 +132,57 @@ mod tests {
 		list.peek_mut().map(|elem| { *elem = 4 });
 		assert_eq!(list.peek(), Some(&4));
 		assert_eq!(list.peek_mut(), Some(&mut 4));
+	}
+
+	#[test]
+	fn into_inter() {
+		let mut list = SList::<i32>::new();
+
+		list.push(1);
+		list.push(2);
+		list.push(3);
+
+		let mut iter = list.into_iter();
+
+		assert_eq!(iter.next(), Some(3));
+		assert_eq!(iter.next(), Some(2));
+		assert_eq!(iter.next(), Some(1));
+		assert_eq!(iter.next(), None);
+	}
+
+	#[test]
+	fn inter() {
+		let mut list = SList::<i32>::new();
+
+		list.push(1);
+		list.push(2);
+		list.push(3);
+
+		let mut iter = list.iter();
+
+		assert_eq!(iter.next(), Some(&3));
+		assert_eq!(iter.next(), Some(&2));
+		assert_eq!(iter.next(), Some(&1));
+		assert_eq!(iter.next(), None);
+		assert_eq!(list.peek(), Some(&3));
+	}
+	
+	#[test]
+	fn inter_mut() {
+		let mut list = SList::<i32>::new();
+
+		list.push(1);
+		list.push(2);
+		list.push(3);
+
+		let mut iter = list.iter_mut();
+		let first = iter.next();
+
+		assert_eq!(first, Some(&mut 3));
+		assert_eq!(iter.next(), Some(&mut 2));
+		assert_eq!(iter.next(), Some(&mut 1));
+		assert_eq!(iter.next(), None);
+		*first.unwrap() = 4;
+		assert_eq!(list.peek(), Some(&4));
 	}
 }
